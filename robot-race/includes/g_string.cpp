9173a1724edb7cf5b0b71bafbe1f24721f8be4f9
Martin <93053179+mjospovich@users.noreply.github.com>
2022-12-30 11:29:03 +0100

    added new funtions for namespace

--- robot-race/includes/g_string.cpp
+++ robot-race/includes/g_string.cpp
@@ -1,25 +1,76 @@
 #include <iostream>
 
-namespace gs{
+namespace gs
+{
 
-  int strComp(std::string first, std::string second) // takes in 2 strings and returns: 1 - first bigger, -1 - second bigger, 0 - equal
+  // takes in 1 string and returns its length as an int
+  int strLen(std::string str)
   {
-    return;
+    int i = 0;
+    while (str[i] != '\0')
+    {
+      i++;
+    }
+
+    return i;
   }
 
-  int strLen(std::string str)  // takes in 1 string and returns its length as an int
+  // takes in 2 strings and returns: 1 - first bigger, -1 - second bigger, 0 - equal
+  int strComp(std::string first, std::string second)
   {
-    return;
+    if (strLen(first) == strLen(second))
+    {
+      int i = 0;
+
+      while ((first[i] == second[i]))
+      {
+        i++;
+
+        if (i == strLen(first))
+        {
+          return 0;
+        }
+      }
+
+      if (first[i] > second[i])
+      {
+        return 1;
+      }
+      else if (second[i] > first[i])
+      {
+        return -1;
+      }
+      else
+      {
+        std::cout << "This shouldn't happen!" << std::endl;
+        return -69;
+      }
+    }
+    else if (strLen(first) > strLen(second))
+    {
+      return 1;
+    }
+    else
+    {
+      return -1;
+    }
   }
 
-  std::string strRev(std::string str) // takes in 1 string and returns it reversed, it doesn't modify the first string
+  // takes in 1 string and returns it reversed, it doesn't modify the first string
+  std::string strRev(std::string str)
   {
-    return;
+    int len = strLen(str);
+    std::string reversed = str;
+
+    reverse(reversed.begin(), reversed.end());
+
+    return reversed;
   }
 
-  void strCopy(std::string destination, std::string source) // takes in two strings and copies second into the first, returns nothing
+  // takes in two strings and copies second into the first, returns nothing
+  void strCopy(std::string destination, std::string source)
   {
-
+    destination = source;
   }
-  
+
 }
