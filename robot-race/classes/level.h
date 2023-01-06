ab16118259354f9b7c73b06dce8d3d60b8a69279
Martin <93053179+mjospovich@users.noreply.github.com>
2023-01-06 11:08:15 +0100

    added new class atributes and updated constructors to support them

--- robot-race/classes/level.h
+++ robot-race/classes/level.h
@@ -11,16 +11,21 @@ class Field{
     char symbol;
     bool isEnd;
     bool isEmpty;
+    bool isRobot;
+    bool isWall;
+    int coords[2];
 
   public:
     Field();
-    Field(char newSymbol);
+    Field(char newSymbol, int x_coord, int y_coord);
     Field(Field& secondObj);
     //~Field();
   
     void swapObj(Field& a, Field& b);
     bool getIsEmpty();
     bool getIsEnd();
+    bool getIsWall();
+    bool getIsRobot();
     char getSymbol();
     void setSymbol(char newSymbol);
 
--- robot-race/level.cpp
+++ robot-race/level.cpp
@@ -3,36 +3,67 @@
 
 Field::Field()
 {
-  symbol = ' ';
+  symbol = '?';
   isEnd = false;
   isEmpty = true;
+  isRobot = false;
+  isWall = false;
+  coords[0] = 0, coords[1] = 1;
 }
 
-Field::Field(char newSymbol)
+Field::Field(char newSymbol, int x_coord, int y_coord)
 {
+  coords[0] = x_coord;
+  coords[1] = y_coord;
+
   symbol = newSymbol;
-  
-  if(symbol == '$'){
+
+  if (symbol == '$')
+  {
     isEnd = true;
   }
-  else{
+  else
+  {
     isEnd = false;
+  }
+
+  if (symbol == '#')
+  {
+    isWall = true;
+  }
+  else
+  {
+    isWall = false;
+  }
+
+  isRobot = false;
+  for(auto name : robotNames)
+  {
+    if(symbol == name)
+    {
+      isRobot = true;
+    }
   }
 
   isEmpty = false;
 }
 
-  Field::Field(Field& secondObj)
-  {
-    symbol = secondObj.symbol;
-    isEnd = secondObj.isEnd;
-    isEmpty = secondObj.isEmpty;
-  }
+Field::Field(Field &secondObj)
+{
+  symbol = secondObj.symbol;
+  isEnd = secondObj.isEnd;
+  isEmpty = secondObj.isEmpty;
+  isRobot = secondObj.isRobot;
+  isWall = secondObj.isWall;
+  
+  coords[0] = secondObj.coords[0];
+  coords[1] = secondObj.coords[1];
+}
 
-//Field::~Field(){;}
+// Field::~Field(){;}
 
 //*used in mainloop for swapping current symbols
-void Field::swapObj(Field& a, Field& b)
+void Field::swapObj(Field &a, Field &b)
 {
   Field temp = a;
   a = b;
@@ -49,28 +80,35 @@ bool Field::getIsEnd()
   return isEnd;
 }
 
+bool Field::getIsWall()
+{
+  return isWall;
+}
+
+bool Field::getIsRobot()
+{
+  return isRobot;
+}
+
 char Field::getSymbol()
 {
   return symbol;
 }
 
-void Field::setSymbol(char newSymbol) //used for adding robots to lvl after loading it (not used in mainloop)
+void Field::setSymbol(char newSymbol) // used for adding robots to lvl after loading it (not used in mainloop)
 {
   //*only able to change symbol if inserting a robot
-  for(auto name : robotNames)
+  for (auto name : robotNames)
   {
     if (newSymbol == name)
     {
       symbol = newSymbol;
       break;
     }
-
   }
-  //for testing, remove/change after 
+  // for testing, remove/change after
   if (newSymbol != symbol)
   {
-      std::cout<<"Not able to change the symbol!" << std::endl;
+    std::cout << "Not able to change the symbol!" << std::endl;
   }
-  
-}    
-
+}
